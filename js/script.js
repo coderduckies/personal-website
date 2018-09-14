fa4ad60332f6a043d36f8d340f1bc12844973afc
Charles Nguyen <charles.nguyen@synacor.com>
2018-09-13 20:23:28 -0700

    added a grid for the cv, and animated header

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 console.log('Yes, I have a few "easter eggs" here.');
-console.log('Try running the "help()" function for the following parameters');
+console.log('Try running the "siteHelp()" function for the following parameters');
 console.log('"lastname"');
 var lastname = "lastname";
-function help(input) {
+function siteHelp(input) {
 	switch(input) {
 		case "lastname":
 			console.log("I've found it important that people can pronounce my last name as it's often pronounced incorrectly. You can read more about it at the wikipedia page here. https://en.wikipedia.org/wiki/Nguyen#Pronunciation");
